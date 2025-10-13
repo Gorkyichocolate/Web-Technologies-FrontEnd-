fa2c85af3a6a414ec6c50fb612b59b06b94aa4e0
Gorkiy Chocolate <beybagame@gmail.com>
2025-10-13 19:25:16 +0500

    1232435

---
+++ html/script.js
@@ -0,0 +1,8 @@
+let rpwd= getElementById("rpwd")
+let pwd= getElementById("pwd")
+let btl = getElementById("buttonlogin").onclick
+if(btl && rpwd != pwd){
+    alert("Error!")
+} else{
+    alert("Good")
+}
